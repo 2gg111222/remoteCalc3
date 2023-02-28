dd4731954e3234aeb938767cc757fbc599461a7d
2gg111222 <2gg111222@gmail.com>
2023-02-28 10:48:43 +0900

    CalCommit2

--- myProject3.py
+++ myProject3.py
@@ -1,9 +1,20 @@
+num1, num2, result = 100,200, 0
+
+
 def add_func (n1, n2):
     res = n1+ n2
     return res
 
-num1, num2, result = 100,200, 0
-
 
 result = add_func(num1, num2)
 print( num1 , '+', num2, '=', result)
+
+
+
+def minus_func (n1, n2):
+    res = n1- n2
+    return res
+
+
+result = minus_func(num1, num2)
+print( num1 , '-', num2, '=', result)
